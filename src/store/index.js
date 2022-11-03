22a56d906a0e538430490573c876977fd9586770
Hari <harikrishkk@gmail.com>
2022-11-03 02:13:07 -0400

    commit 4: add custom enhancer, and dev tools enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import { legacy_createStore as createStore } from 'redux';
 import { combineReducers } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import productsReducer from './products';
+import { addLoggingOnDispatch } from '../add-ons/enhancers';
 
 const rootReducer = combineReducers({
   products: productsReducer,
 });
 
 // create the store
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(addLoggingOnDispatch)
+);
 export default store;
