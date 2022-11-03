f4d0ec59aff675f3cc339cbd2c656088b9383ca7
Hari <harikrishkk@gmail.com>
2022-11-03 01:20:28 -0400

    commit 3: use combineReducer, seperate products into its own slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,10 @@
 import { legacy_createStore as createStore } from 'redux';
-import { v4 as uuidv4 } from 'uuid';
+import { combineReducers } from 'redux';
+import productsReducer from './products';
 
-// Initial state object
-const INITIAL_STATE = {
-  products: [],
-};
-
-const PRODUCTS = [
-  {
-    id: uuidv4(),
-    text: 'iPhone',
-    isFavorite: true,
-    category: 'Mobile',
-    price: 1900,
-  },
-  {
-    id: uuidv4(),
-    text: 'Macbook Pro',
-    isFavorite: false,
-    category: 'Laptop',
-    price: 2900,
-  },
-  {
-    id: uuidv4(),
-    text: 'Peter England',
-    isFavorite: false,
-    category: 'Clothing',
-    price: 299,
-  },
-];
-
-function rootReducer(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case 'products/LOAD_PRODUCTS':
-      return {
-        ...state,
-        products: [...PRODUCTS],
-      };
-    default:
-      return state;
-  }
-}
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
 
 // create the store
 const store = createStore(rootReducer);
