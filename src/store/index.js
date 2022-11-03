cb84bb8cf9f2fbb607071422836715f02e2c6774
Hari <harikrishkk@gmail.com>
2022-11-02 22:32:04 -0400

    commit 1: install packages, create store with product slice

---
+++ src/store/index.js
@@ -0,0 +1,47 @@
+import { legacy_createStore as createStore } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
+
+// Initial state object
+const INITIAL_STATE = {
+  products: [],
+};
+
+const PRODUCTS = [
+  {
+    id: uuidv4(),
+    text: 'iPhone',
+    isFavorite: true,
+    category: 'Mobile',
+    price: 1900,
+  },
+  {
+    id: uuidv4(),
+    text: 'Macbook Pro',
+    isFavorite: false,
+    category: 'Laptop',
+    price: 2900,
+  },
+  {
+    id: uuidv4(),
+    text: 'Peter England',
+    isFavorite: false,
+    category: 'Clothing',
+    price: 299,
+  },
+];
+
+function rootReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'products/LOAD_PRODUCTS':
+      return {
+        ...state,
+        products: [...PRODUCTS],
+      };
+    default:
+      return state;
+  }
+}
+
+// create the store
+const store = createStore(rootReducer);
+export default store;
