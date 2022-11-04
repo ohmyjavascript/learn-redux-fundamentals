aaae84c2d512c9ae1a971b680ea1b809400ad520
Hari <harikrishkk@gmail.com>
2022-11-04 03:28:01 -0400

    commit 9: add new product , update products slice

--- src/store/products.js
+++ src/store/products.js
@@ -34,7 +34,21 @@ function productsReducer(state = INITIAL_STATE, action) {
     case 'products/LOAD_PRODUCTS':
       return {
         ...state,
-        products: [...PRODUCTS],
+        products: state.products.length === 0 ? [...PRODUCTS] : state.products,
+      };
+
+    case 'product/ADD_PRODUCT':
+      const { title, price, category } = action.payload;
+      const newProduct = {
+        text: title,
+        price,
+        category,
+        id: uuidv4(),
+        isFavorite: false,
+      };
+      return {
+        ...state,
+        products: [...state.products, newProduct],
       };
     default:
       return state;
