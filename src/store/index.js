a6a7f1f1ef3d37e1be419ba867abeb5b4cae4abe
Hari <harikrishkk@gmail.com>
2022-11-05 03:53:05 -0400

    commit 14: add redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,22 @@ import { combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import productsReducer from './products';
 import favoritesReducer from './favorites';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['products'],
+};
 
 const rootReducer = combineReducers({
   products: productsReducer,
   favorites: favoritesReducer,
 });
 
+const peristedReducer = persistReducer(persistConfig, rootReducer);
+
 // create the store
-const store = createStore(rootReducer, composeWithDevTools());
-export default store;
+export const store = createStore(peristedReducer, composeWithDevTools());
+export const persistor = persistStore(store);
