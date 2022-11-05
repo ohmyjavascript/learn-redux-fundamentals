e925d0117528e1150689452430d2e4ba6eac55ad
Hari <harikrishkk@gmail.com>
2022-11-05 00:50:31 -0400

    commit 12: add favorite slice  & insert product id

---
+++ src/store/favorites.js
@@ -0,0 +1,16 @@
+const INIT_STATE = [];
+
+function favoritesReducer(state = INIT_STATE, action) {
+  switch (action.type) {
+    case 'favorites/ADD_FAVORITE':
+      const itemID = state.includes(action.payload);
+      if (!itemID) {
+        return [...state, action.payload];
+      }
+      return state.filter((item) => item !== action.payload);
+    default:
+      return state;
+  }
+}
+
+export default favoritesReducer;
