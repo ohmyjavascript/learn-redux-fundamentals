ba03b9e46b17b188ed58576d07b0a3995b2ba581
Hari <harikrishkk@gmail.com>
2022-11-14 00:48:05 -0500

    commit 38: wire-cart items, add notifications

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,20 @@ import React from 'react';
 import CartItem from 'components/CartItem';
 import { useSelector } from 'react-redux';
 import { selectCartItems, selectCartTotal } from 'store/cart';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
+
+  if (cartItems.length === 0) {
+    return (
+      <div className="alert alert-dismissable alert-info">
+        <strong>Alert!</strong> Please start adding items to cart
+        <Link to="/"> from here </Link>
+      </div>
+    );
+  }
 
   return (
     <ul className="list-group">
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -2,9 +2,19 @@ import React from 'react';
 import FavoriteItem from 'components/FavoriteItem';
 import { useSelector } from 'react-redux';
 import { selectFavorites } from 'store/favorites/selectors';
+import { Link } from 'react-router-dom';
 
 const Favorites = () => {
   const favoriteItems = useSelector(selectFavorites);
+
+  if (favoriteItems.length === 0) {
+    return (
+      <div className="alert alert-dismissable alert-info">
+        <strong>Alert!</strong> Please start adding items as favorites
+        <Link to="/"> from here </Link>
+      </div>
+    );
+  }
   return (
     <ul className="list-group">
       {favoriteItems.map((item) => (
